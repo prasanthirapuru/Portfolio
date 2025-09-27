42711d0c2fe3eb6ff2f0c44bac63c9599e6fd597
prasanthirapuru <prasanthirapuru91@gmail.com>
2025-09-27 19:52:30 +0530

    Update Home2.js

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -19,36 +19,36 @@ function Home2() {
               LET ME <span className="purple"> INTRODUCE </span> MYSELF
             </h1>
             <p className="home-about-body">
-              I fell in love with programming and I have learned quite a lot
-              along the way, I think… 🤷‍♂️
+              I fell in love with programming and I have at least learnt
+              something, I think… 🤷‍♂️
               <br />
               <br />I am fluent in classics like
               <i>
-                <b className="purple"> Python, JavaScript, and Java. </b>
+                <b className="purple"> C++, Javascript and Go. </b>
               </i>
               <br />
               <br />
-              My fields of interest include building new &nbsp;
+              My field of Interest's are building new &nbsp;
               <i>
-                <b className="purple">Web Technologies </b> and
-                working on areas related to{" "}
+                <b className="purple">Web Technologies and Products </b> and
+                also in areas related to{" "}
                 <b className="purple">
-                  Data Science.
+                  Blockchain.
                 </b>
               </i>
               <br />
               <br />
-              Whenever possible, I apply my passion for developing products
-              with
+              Whenever possible, I also apply my passion for developing products
+              with <b className="purple">Node.js</b> and
               <i>
                 <b className="purple">
                   {" "}
-                  Machine Learning using Python libraries
+                  Modern Javascript Library and Frameworks
                 </b>
               </i>
-              &nbsp; such as
+              &nbsp; like
               <i>
-                <b className="purple"> NumPy, Pandas, Scikit-learn, and Matplotlib</b>
+                <b className="purple"> React.js and Next.js</b>
               </i>
             </p>
           </Col>
@@ -67,23 +67,42 @@ function Home2() {
             <ul className="home-about-social-links">
               <li className="social-icons">
                 <a
-                  href="https://github.com/prasanthirapuru"
+                  href="https://github.com/soumyajit4419"
                   target="_blank"
                   rel="noreferrer"
-                  className="icon-colour home-social-icons"
+                  className="icon-colour  home-social-icons"
                 >
                   <AiFillGithub />
                 </a>
               </li>
-
               <li className="social-icons">
                 <a
-                  href="https://www.linkedin.com/in/prasanthi-rapuru-ba7ab8370"
+                  href="https://twitter.com/Soumyajit4419"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <AiOutlineTwitter />
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="https://www.linkedin.com/in/soumyajit4419/"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <FaLinkedinIn />
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="https://www.instagram.com/soumyajit4419"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour home-social-icons"
                 >
-                  <FaLinkedinIn />
+                  <AiFillInstagram />
                 </a>
               </li>
             </ul>
@@ -93,5 +112,4 @@ function Home2() {
     </Container>
   );
 }
-
 export default Home2;
