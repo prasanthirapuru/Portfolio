ef56ae14bd7ebaddbd257b7eecfffb0dc00ea786
prasanthirapuru <prasanthirapuru91@gmail.com>
2025-09-27 20:07:59 +0530

    Update Techstack.js

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -1,58 +1,30 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
-import { CgCPlusPlus } from "react-icons/cg";
-import {
-  DiJavascript1,
-  DiReact,
-  DiNodejs,
-  DiMongodb,
-  DiPython,
-  DiGit,
-  DiJava,
-} from "react-icons/di";
-import {
-  SiRedis,
-  SiFirebase,
-  SiNextdotjs,
-  SiSolidity,
-  SiPostgresql,
-} from "react-icons/si";
-import { TbBrandGolang } from "react-icons/tb";
+import { DiPython } from "react-icons/di";
+import { SiNumpy, SiPandas, SiScikitlearn } from "react-icons/si";
 
 function Techstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
-      
-      <Col xs={4} md={2} className="tech-icons">
-        <DiJavascript1 />
-      </Col>
-     
-     
-      <Col xs={4} md={2} className="tech-icons">
-        <DiReact />
-      </Col>
-      
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiMongodb />
-      </Col>
-      
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiGit />
-      </Col>
-      
-      </Col>
-     
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiPostgresql />
-      </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiPython />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <DiJava />
+        <SiNumpy />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiPandas />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiScikitlearn />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiPython /> {/* placeholder for Matplotlib */}
+        <p style={{ fontSize: "0.8em" }}>Matplotlib</p>
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiPython /> {/* placeholder for Seaborn */}
+        <p style={{ fontSize: "0.8em" }}>Seaborn</p>
       </Col>
     </Row>
   );
