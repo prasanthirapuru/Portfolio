f4324570251b410f4b4468d6c371c4b0a049e37b
prasanthirapuru <prasanthirapuru91@gmail.com>
2025-09-29 13:25:43 +0530

    Update Techstack.js

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -1,31 +1,80 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
+
+// Data Science Icons
 import { DiPython } from "react-icons/di";
 import { SiNumpy, SiPandas, SiScikitlearn } from "react-icons/si";
+import { MdOutlineShowChart } from "react-icons/md"; // For Matplotlib
+import { FiBarChart } from "react-icons/fi"; // For Seaborn
+
+// NEW: Web Development and General Programming Icons
+import { DiJavascript1, DiJava, DiCss3, DiHtml5 } from "react-icons/di"; 
 
 function Techstack() {
   return (
+    // Ensure you are using a consistent class name for styling
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+
+      {/* ==================================== */}
+      {/* NEW ICONS: Web Dev & General Programming */}
+      {/* ==================================== */}
+      
+      {/* JavaScript */}
+      <Col xs={4} md={2} className="tech-icons">
+        <DiJavascript1 />
+      </Col>
+
+      {/* Java */}
+      <Col xs={4} md={2} className="tech-icons">
+        <DiJava />
+      </Col>
+
+      {/* CSS */}
+      <Col xs={4} md={2} className="tech-icons">
+        <DiCss3 />
+      </Col>
+
+      {/* HTML */}
+      <Col xs={4} md={2} className="tech-icons">
+        <DiHtml5 />
+      </Col>
+      
+      {/* ==================================== */}
+      {/* Existing Data Science Icons */}
+      {/* ==================================== */}
+
+      {/* Python */}
       <Col xs={4} md={2} className="tech-icons">
         <DiPython />
       </Col>
+
+      {/* NumPy */}
       <Col xs={4} md={2} className="tech-icons">
         <SiNumpy />
       </Col>
+
+      {/* Pandas */}
       <Col xs={4} md={2} className="tech-icons">
         <SiPandas />
       </Col>
+
+      {/* Scikit-learn */}
       <Col xs={4} md={2} className="tech-icons">
         <SiScikitlearn />
       </Col>
+
+      {/* Matplotlib (Visualization Icon) */}
       <Col xs={4} md={2} className="tech-icons">
-        <DiPython /> {/* placeholder for Matplotlib */}
-        <p style={{ fontSize: "0.8em" }}>Matplotlib</p>
+        <MdOutlineShowChart />
+        <p style={{ fontSize: "0.8em", marginTop: "5px" }}>Matplotlib</p>
       </Col>
+
+      {/* Seaborn (Visualization Icon) */}
       <Col xs={4} md={2} className="tech-icons">
-        <DiPython /> {/* placeholder for Seaborn */}
-        <p style={{ fontSize: "0.8em" }}>Seaborn</p>
+        <FiBarChart />
+        <p style={{ fontSize: "0.8em", marginTop: "5px" }}>Seaborn</p>
       </Col>
+      
     </Row>
   );
 }
