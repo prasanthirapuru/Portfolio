c5385db544378e2ff778291b315e8473aa90ce6a
prasanthirapuru <prasanthirapuru91@gmail.com>
2025-09-29 13:36:10 +0530

    Update Toolstack.js

--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -1,31 +1,55 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
+
+// Icons from 'si' (Simple Icons)
 import {
-  SiVisualstudiocode,
-  SiPostman,
-  SiSlack,
-  SiVercel,
-  SiMacos,
+  SiVisualstudiocode, 
+  SiSlack,            
+  SiStreamlit,        
+  SiVercel,           
+  SiGooglecolab,     
 } from "react-icons/si";
+
+// Icons from 'di' (Devicons)
+import {
+  DiGithubBadge,      // NEW: GitHub
+} from "react-icons/di";
+
 
 function Toolstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiMacos />
-      </Col>
+      
+      {/* Visual Studio Code */}
       <Col xs={4} md={2} className="tech-icons">
         <SiVisualstudiocode />
       </Col>
+      
+      {/* GitHub (New addition) */}
       <Col xs={4} md={2} className="tech-icons">
-        <SiPostman />
+        <DiGithubBadge />
       </Col>
+      
+      {/* Slack */}
       <Col xs={4} md={2} className="tech-icons">
         <SiSlack />
       </Col>
+      
+      {/* Vercel */}
       <Col xs={4} md={2} className="tech-icons">
         <SiVercel />
       </Col>
+      
+      {/* Streamlit */}
+      <Col xs={4} md={2} className="tech-icons">
+        <SiStreamlit />
+      </Col>
+
+      {/* Google Colab (New addition, replacing Google Cloud) */}
+      <Col xs={4} md={2} className="tech-icons">
+        <SiGooglecolab />
+      </Col>
+      
     </Row>
   );
 }
