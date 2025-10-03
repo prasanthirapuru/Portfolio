abd35a59ed47917ced8d910f47ef211e7b56bf00
prasanthirapuru <prasanthirapuru91@gmail.com>
2025-10-03 20:43:57 +0530

    Update Contact.js

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,11 +1,11 @@
 import React from "react";
-import "./sendbuttons.css";
+import "./sendbuttons.css"; // make sure the path is correct
 
 function Contact() {
   return (
     <div className="contact-page">
       <div className="contact-card">
-        <h2 className="contact-heading">Contact Me</h2> {/* Added heading */}
+       <h2 className="contact-heading">Contact Me</h2>
         <form className="contact-form">
           <input type="text" placeholder="Your Name" />
           <input type="email" placeholder="Your Email" />
