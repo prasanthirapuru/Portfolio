ebe9a8d9c4fc46418250b06d290ea2167607d53d
prasanthirapuru <prasanthirapuru91@gmail.com>
2025-10-04 12:26:19 +0530

    Update Projects.js

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -6,7 +6,7 @@ import leaf from "../../Assets/Projects/leaf.png";
 import emotion from "../../Assets/Projects/emotion.png";
 import editor from "../../Assets/Projects/codeEditor.png"; 
 import chatify from "../../Assets/Projects/chatify.png"; 
-import suicide from "../../Assets/Projects/suicide.png";
+import suicide from "../../Assets/Projects/suicide.png"; 
 import waterqualityprediction from "../../Assets/Projects/waterqualityprediction.png";
 
 function Projects() {
